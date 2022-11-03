15efbbd8386f4925afcc8034a52da890c81baef2
HugoSanche <hbmclaren@gmail.com>
2022-11-03 12:43:29 -0600

    Finish Adding notes into array and put this notes in a dinamyc component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,31 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes"
+
+/*
+function createNotes(notes){
+  return (
+    <Note 
+    llave={notes.key}
+    titulo={notes.title}
+    contenido={notes.content}
+    />
+  );
+}
+*/
 function App() {
   return <div>
         <Header />
+        {notes.map (notes => ( //Using Arrow .-when you only use one component (Note) like this example you could quit "return"
+            <Note 
+            llave={notes.key}
+            titulo={notes.title}
+            contenido={notes.content}
+            />
+          ))} 
         <Footer />
-        <Note />
+        
         </div>
 
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,9 @@ import React from "react";
 var myDate = new Date();
 myDate =myDate.getFullYear();
 
-var x="Hola;"
 function Footer(){
     return (
-        <footer><p>CopyRight:{myDate}</p>
+        <footer><p>CopyRight: ⓒ  {myDate}</p>
         </footer>
       );
 }
